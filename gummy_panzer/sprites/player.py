f647bcea5993af208db499fa79abdfb5e6d5e9b9
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-13 20:24:42 +0000

    Got it to closer to what i want for movement
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@51 aaafb41a-2973-0410-b970-db74e9e91ae2

--- gummy_panzer/sprites/player.py
+++ gummy_panzer/sprites/player.py
@@ -4,46 +4,71 @@ from . import util
 ACCEL = 2
 MAX_V = 10
 
+class _MovingState(object):
+    STOPPED = 0
+    PLUS = 1
+    MINUS = -1
+
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, *groups):
         pygame.sprite.Sprite.__init__(self, *groups)
         self.image = util.load_image("player.png")
         self.rect = self.image.get_rect()
-        class PhysicsProperty(object):
+        self._ms_x = _MovingState.STOPPED
+        self._ms_y = _MovingState.STOPPED
+        class _Velocity(object):
             x = 0
             y = 0
-        self.v = PhysicsProperty()
-        self.a = PhysicsProperty()
+        self._velocity = _Velocity()
 
     def move_up(self):
-        self.a.y = -ACCEL
+        self._ms_y = _MovingState.MINUS
 
     def move_down(self):
-        self.a.y = ACCEL
-
-    def stop_y(self):
-        self.a.y = 0
-
-    def stop_x(self):
-        self.a.x = 0
-
-    def move_right(self):
-        self.a.x = ACCEL
+        self._ms_y = _MovingState.PLUS
 
     def move_left(self):
-        self.a.x = -ACCEL
+        self._ms_x = _MovingState.MINUS
+
+    def move_right(self):
+        self._ms_x = _MovingState.PLUS
+
+    @staticmethod
+    def _get_physics(current_position, current_velocity, moving_state):
+        """Returns (new velocity, new x)"""
+
+        if moving_state == _MovingState.STOPPED:
+            if current_velocity > 0:
+                acceleration = -ACCEL
+            elif current_velocity < 0:
+                acceleration = ACCEL
+            else:
+                acceleration = 0
+        elif moving_state == _MovingState.PLUS:
+            acceleration = ACCEL
+        else:
+            acceleration = -ACCEL
+
+        current_position += current_velocity
+        velocity = min(MAX_V, current_velocity + acceleration)
+        velocity = max(-MAX_V, velocity + acceleration)
+        if (moving_state == _MovingState.STOPPED and
+                (velocity > 0 and current_velocity < 0 ) or
+                (velocity < 0 and current_velocity > 0)):
+            velocity = 0
+        return (velocity, current_position)
 
     def update(self):
         x, y = self.rect.topleft
-        x += self.v.x
-        y += self.v.y
 
-        self.v.x = min(MAX_V, self.v.x + self.a.x)
-        self.v.x = max(-MAX_V, self.v.x)
-        self.v.y = min(MAX_V, self.v.y + self.a.y)
-        self.v.y = max(-MAX_V, self.v.y)
+        self._velocity.x, x = Player._get_physics(x,
+                                                  self._velocity.x,
+                                                  self._ms_x)
+        self._velocity.y, y = Player._get_physics(y,
+                                                  self._velocity.y,
+                                                  self._ms_y)
 
-        self.a.x, self.a.y = 0, 0
-
+        self._ms_x = _MovingState.STOPPED
+        self._ms_y = _MovingState.STOPPED
         self.rect.topleft = x, y
