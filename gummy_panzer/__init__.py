ebec4c718d1cd115d3574d9fe654bbf4badca006
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-16 21:11:29 +0000

    Draw the hud
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@271 aaafb41a-2973-0410-b970-db74e9e91ae2

--- gummy_panzer/__init__.py
+++ gummy_panzer/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import pygame
 from . import settings
-from .sprites import player
+from .sprites import player, hud
 
 def main(argv):
     pygame.init()
@@ -11,6 +11,7 @@ def main(argv):
     pygame.display.set_caption('Roflmao test')
     clock = pygame.time.Clock()
     my_player = player.Player()
+    my_hud = hud.Hud(100, 0, 0, 0)
     extra_sprites = []
     while True:
         clock.tick(settings.FRAMES_PER_SECOND)
@@ -25,6 +26,7 @@ def main(argv):
         else:
             extra_sprites.extend(my_player.update())
             screen.blit(my_player.image, my_player.rect.topleft)
+            my_hud.draw_hud(screen)
             for sprite in extra_sprites:
                 sprite.update()
                 screen.blit(sprite.image, sprite.rect.topleft)
