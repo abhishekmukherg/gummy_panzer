a2c0a7f67595f5f0f39fc95dcd5b78a0101e18fc
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-16 21:11:29 +0000

    Fixed a few syntax errors
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@270 aaafb41a-2973-0410-b970-db74e9e91ae2

--- gummy_panzer/sprites/hud.py
+++ gummy_panzer/sprites/hud.py
@@ -11,14 +11,14 @@ class Hud(object):
     def draw_hud(self, surf):
         font = pygame.font.Font(None, 20)
 
-        surfscore = font.render("Score:%d" % self.score, 1, (0, 0, 0))
-        surf.blit(surfscore, 600, 0)
+        surfscore = font.render("Score:%d" % self.score, 1, (255, 0, 0))
+        surf.blit(surfscore, (600, 0))
 
-        surftime = font.render("Time:%d" % self.time, 1, (0, 0, 0))
-        surf.blit(surftime, 400, 0)
+        surftime = font.render("Time:%d" % self.time, 1, (255, 0, 0))
+        surf.blit(surftime, (400, 0))
 
         surfenergy = font.render("EN:%d" % self.energy, 1, (0, 255, 0))
-        surf.blit(surfenergy, 200, 0)
+        surf.blit(surfenergy, (200, 0))
 
         surfhealth = font.render("HP:%d" % self.health, 1, (0, 0, 255))
-        surf.blit(surfhealth, 0, 0)
+        surf.blit(surfhealth, (0, 0))
