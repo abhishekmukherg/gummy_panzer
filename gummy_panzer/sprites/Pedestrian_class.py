06d06a40b0153cb22f153cf79de704932a59fe2a
housew <housew@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-16 18:22:31 +0000

    adding pedestrian class
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@101 aaafb41a-2973-0410-b970-db74e9e91ae2

---
+++ gummy_panzer/sprites/Pedestrian_class.py
@@ -0,0 +1,78 @@
+class Pedestrian(object):
+
+	def __init__(self, species, animation, splatter, height, width):
+		self.species = species         #species has two states. 0 is human. 1 is alien.
+		self.animation = animation     #animation has four states. 0 is standing/shooting. 1 is running left. 2 is running right. 3 is being beamed up.
+		self.splatter = splatter        #splatter has two states. 0 is unsplatted. 1 is splatted.
+		self.deapth = height           #deapth to control which people appear on top
+		self.width = width
+		pygame.sprite.Sprite.__init__(self)
+		if species == 0:
+			#human sprites
+		elif species == 1:
+			#alien sprites
+		
+	def beam_me_up(self):
+		if self.splatter == 1:
+			break
+		elif self.animation == 3:
+			break
+		else:
+			return 3
+			
+	def splat_me(self):
+		if self.splatter == 1:
+			break
+		elif self.animation == 3:
+			break
+		else:
+			if self.species == 0:
+				#score = score - 5		#players could lose points for having humans die. currently commented out
+			return (1, self.deapth + 20)     #return a value for splatter and a modification for deapth so that blood always appears below non-splattered
+	
+	def run_around(self):		#function to change the horizontal location of pedestrians
+		if self.splatter == 1:
+			return -3           #function will return the distance to move the object to the left
+		if self.animation == 1:
+		    return -5           #returns a larger number for units that are running left
+		if self.animation == 2:
+			return -1
+		return -3
+	
+	def is_on_screen(self):
+		if width + 4 =< screenleftside
+			self.delete()
+			
+	def draw_person(self):
+		if self.splatter == 1:
+			return "filename"
+		elif self.species == 0:
+			if self.animation == 0:
+				return "filename3"
+			elif self.animation == 1:
+				return "filename4"
+			elif self.animation == 2:
+				return "filename5"
+			else self.animation == 3:
+				return "filename6"
+		else:
+			if self.animation == 0:
+				return "filename7"
+			elif self.animation == 1:
+				return "filename8"
+			elif self.animation == 2:
+				return "filename9"
+			else self.animation == 3:
+				return "filename10"
+
+class Alien(Pedestrian):
+
+	def __init__(self):
+		Pedestrian.__init__(self)
+				
+class Human(Pedestrian):
+
+	def __init__(self):
+		Pedestrian.__init__(self)				
+				
+				
