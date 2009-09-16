a98c16eb4e224b16979052fac2892131c3f9c03e
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-16 21:11:28 +0000

    Threw hud.py through pylint
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@269 aaafb41a-2973-0410-b970-db74e9e91ae2

--- gummy_panzer/sprites/hud.py
+++ gummy_panzer/sprites/hud.py
@@ -1,8 +1,6 @@
 import pygame
-from . import util
-from .. import settings
 
-class hud(object):
+class Hud(object):
 
     def __init__(self, health, energy, score, time):
         self.health = health
@@ -11,12 +9,16 @@ class hud(object):
         self.time = time        
 
     def draw_hud(self, surf):
-        f = pygame.font.Font(None, 20)
-        surfscore = f.render("Score:%d"%self.score, 1, (0,0,0))
+        font = pygame.font.Font(None, 20)
+
+        surfscore = font.render("Score:%d" % self.score, 1, (0, 0, 0))
         surf.blit(surfscore, 600, 0)
-        surftime = f.render("Time:%d"%self.time, 1, (0,0,0)
+
+        surftime = font.render("Time:%d" % self.time, 1, (0, 0, 0))
         surf.blit(surftime, 400, 0)
-        surfenergy = f.render("EN:%d"%self.energy, 1, (0, 255, 0)
+
+        surfenergy = font.render("EN:%d" % self.energy, 1, (0, 255, 0))
         surf.blit(surfenergy, 200, 0)
-        surfhealth = f.render("HP:%d"%self.health, 1, (0, 0, 255)
+
+        surfhealth = font.render("HP:%d" % self.health, 1, (0, 0, 255))
         surf.blit(surfhealth, 0, 0)
