e45652817104935fd330e0b4ab6103d6d2df5023
mukhea2 <mukhea2@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-18 21:22:49 +0000

    Temp commit, it's psychadelic
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@338 aaafb41a-2973-0410-b970-db74e9e91ae2

--- gummy_panzer/__init__.py
+++ gummy_panzer/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 
 import logging
+logging.basicConfig(level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
 
 import pygame
 from . import settings
-from .sprites import player, enemies, hud
+from .sprites import player, hud, effects, util
 
 def main(argv):
     pygame.init()
@@ -36,6 +37,8 @@ def main(argv):
         else:
             extra_sprites.extend(my_player.update())
             screen.blit(my_player.image, my_player.rect.topleft)
+            screen.blit(effect.image, effect.rect, effect.draw_area)
+            effect.anim_frame = (effect.anim_frame + 1) % 6
             my_hud.draw_hud(screen)
             for sprite in extra_sprites:
                 sprite.update()
