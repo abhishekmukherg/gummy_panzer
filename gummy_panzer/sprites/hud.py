c67029b3c0d9e206a45cddbbcfc96e252e314ea4
housew <housew@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-18 21:32:33 +0000

    added energy bar
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@351 aaafb41a-2973-0410-b970-db74e9e91ae2

--- gummy_panzer/sprites/hud.py
+++ gummy_panzer/sprites/hud.py
@@ -11,6 +11,8 @@ class Hud(object):
         self.time = time
         self.surf_hp = pygame.Surface((180, 16))
         self.rect_hp = pygame.Rect(30, 0, 100, 15) 
+        self.surf_en = pygame.Surface((180, 16))
+        self.rect_en = pygame.Rect(30, 0, 100, 15)
 
     def draw_hud(self, surf):
         font = pygame.font.Font(None, 20)
@@ -21,13 +23,18 @@ class Hud(object):
         surftime = font.render("Time:%d" % self.time, 1, (255, 0, 0))
         surf.blit(surftime, (400, 0))
 
-        surfenergy = font.render("EN:%d" % self.energy, 1, (0, 255, 0))
+        self.energy = 25
+        temp_rect1 = pygame.Rect(30, 0, self.energy, 15)
+        pygame.draw.rect(self.surf_en, (0, 255, 0), self.rect_en, 1)    #added test code
+        pygame.draw.rect(self.surf_en, (0, 255, 0), temp_rect1, 0)
+        surf.blit(self.surf_en, (205, 0))
+
+        surfenergy = font.render("EN:", 1, (0, 255, 0))
         surf.blit(surfenergy, (200, 0))
 
-        self.health = 85
-        temp_rect = pygame.Rect(30, 0, self.health, 15)
+        temp_rect2 = pygame.Rect(30, 0, self.health, 15)
         pygame.draw.rect(self.surf_hp, (0, 0, 255), self.rect_hp, 1)    #added test code
-        pygame.draw.rect(self.surf_hp, (0, 0, 255), temp_rect, 0)
+        pygame.draw.rect(self.surf_hp, (0, 0, 255), temp_rect2, 0)
         surf.blit(self.surf_hp, (0, 0))
 
         surfhealth = font.render("HP:", 1, (0, 0, 255))
