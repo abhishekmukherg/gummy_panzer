c8b15b6204535d5f7d6741aa6dc4a269c73c5c08
ingulp <ingulp@aaafb41a-2973-0410-b970-db74e9e91ae2>
2009-09-19 18:00:00 +0000

    general building update
    git-svn-id: http://subversion.hss.rpi.edu/svn/COGS4520/gummy_panzer/trunk@379 aaafb41a-2973-0410-b970-db74e9e91ae2

--- gummy_panzer/sprites/buildings.py
+++ gummy_panzer/sprites/buildings.py
@@ -21,45 +21,45 @@ class building(object):
         #Image is the location of the building spritesheet.  Carnageimage is the dust when the building collapses
         #Height should be 0 for below the street, 2 for above the street, 1 for on the street (1 should not be used)
         #state is 0 for whole, 1 for damaged, 2 for destroyed.
-		
-		self.wavex=0    #X position in the wave
-		self.height=2   #Height in pixels of building
-		self.level=0    #Level 0 for below street, 1 for above street
-        self.alive=1    #1 for alive, 2 for falling, 0 for destroyed
-		self.fallspeed=1#How many pixels it falls each loop.
+
+        #self.wavex=0    #X position in the wave
+        self.height=2   #Height in pixels of building
+        self.level=0    #Level 0 for below street, 1 for above street
+        self.fallspeed=1#How many pixels it falls each loop.
         
-	    self.image= pygame.image.load("building.png")
-		self.rect=self.image.get_rect()
-	    
-		self.STATE=0    #State of the building.  0 intact, 1 damaged, 2 destroyed.
+        self.image= pygame.image.load("building.png")
+        self.rect=self.image.get_rect()
+
+
+        self.state=0    #State of the building.  0 intact, 1 damaged, 2 destroyed.
         
         
-	def draw(self, surface):
+        def draw(self, surface):
         #note that when state changes it will make the building sprite change(in this case move over 50 pixels)
         if self.alive ==2:
-            screen.blit(self.image, self.rect, pygame.Rect(50*(self.STATE), 0, 50, 50))  # change this
+            screen.blit(self.image, self.rect, pygame.Rect(50*(self.state), 0, 50, 50))  # change this
         #loop over all enemies,  checking fallers against ground height.  Set enemy to explode, add effects, and all buildings, check if it's in the blast radius.
     def update(self):
         self.rect.move_ip(settings.SCROLL_RATE,0)
-		
+
         if self.rect.contains(explosionrect):
-            self.STATE+=1
-            if self.STATE ==2:
+            self.state+=1
+            if self.state ==2:
                 self.alive=2
         if self.alive==2:
             self.rect.move_ip(0,fallspeed)
             height-=1
-			if height==0:
-			    self.alive=0
+                        if height==0:
+                            self.alive=0
                 
             
             
         self.rect.move_ip(screenmove, 0)
-	
-	#INTACT STATE: 0
-	#DAMAGED STATE: 1
-	#DESTROYED STATE: 2
-	#If state is intact display sprite is 1
-	#If state is damaged display sprite is 2
-	#If state is Destroyed display sprite is 3
-	#If building takes 1 damage, change sprite
+
+        #INTACT STATE: 0
+        #DAMAGED STATE: 1
+        #DESTROYED STATE: 2
+        #If state is intact display sprite is 1
+        #If state is damaged display sprite is 2
+        #If state is Destroyed display sprite is 3
+        #If building takes 1 damage, change sprite
