a5d772f97936b297dee2314b70eed8461e05968a
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-09-21 21:52:55 -0400

    Fixed tractor beam movements

--- gummy_panzer/sprites/tractorbeam.py
+++ gummy_panzer/sprites/tractorbeam.py
@@ -27,48 +27,46 @@ class TractorBeam(pygame.sprite.Sprite):
 
     @property
     def touching_ground(self):
-        return ((self.draw_area.y + self.draw_area.height) >
+        return ((self.rect.y + self.draw_area.height) >
                     (settings.SCREEN_HEIGHT * .95))
 
     def update (self, player):
         if self.extending:
+            LOG.info("loc: (%d,%d), height: %d, extending" %
+                            (self.rect.x, self.rect.y,
+                            self.draw_area.height))
+            self.draw_area.height += EXTEND_SPEED
             if self.touching_ground:
                 LOG.info("loc: (%d,%d), height: %d, done extending" %
-                                (self.draw_area.x, self.draw_area.y,
+                                (self.rect.x, self.rect.y,
                                 self.draw_area.height))
                 self.extending = False
                 self.extended = True
                 self.draw_area.height = ((settings.SCREEN_HEIGHT * .95) -
-                                                            self.draw_area.y)
+                                                            self.rect.y)
                 #draw circle @bottom
-            else:
-                LOG.info("loc: (%d,%d), height: %d, extending" %
-                                (self.draw_area.x, self.draw_area.y,
-                                self.draw_area.height))
-                self.draw_area.height += EXTEND_SPEED
 
         elif self.abducting:
             pass
 
         elif self.retracting:
+            LOG.info("loc: (%d,%d), height: %d, retracting" %
+                            (self.rect.x, self.rect.y,
+                            self.draw_area.height))
+            self.draw_area.height -= EXTEND_SPEED
             if self.draw_area.height <= 0:
                 LOG.info("loc: (%d,%d), height: %d, done retracting" %
-                                (self.draw_area.x, self.draw_area.y,
+                                (self.rect.x, self.rect.y,
                                 self.draw_area.height))
                 self.draw_area.height = 0
                 self.retracting = False
-            else:
-                LOG.info("loc: (%d,%d), height: %d, retracting" %
-                                (self.draw_area.x, self.draw_area.y,
-                                self.draw_area.height))
-                self.draw_area.height -= EXTEND_SPEED
 
         elif self.extended:
             LOG.info("loc: (%d,%d), height: %d, fully extended!" %
-                                (self.draw_area.x, self.draw_area.y,
+                                (self.rect.x, self.rect.y,
                                 self.draw_area.height))
             self.draw_area.height = ((settings.SCREEN_HEIGHT * .95) -
-                                (self.draw_area.y + self.draw_area.height))
+                                (self.rect.top))
         
         self.rect.top = player.rect.bottom - 5
         self.rect.left = player.rect.center[0] - (self.rect.width / 2)
