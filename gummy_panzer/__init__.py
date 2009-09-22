2d431a9aedf6d709aea459a252c8567655a59e9c
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-09-22 16:10:30 -0400

    Changed some tabs back into spaces

--- gummy_panzer/__init__.py
+++ gummy_panzer/__init__.py
@@ -191,16 +191,16 @@ class Game(object):
 
     def _draw(self):
         self.__draw_background(self.background1_pos, self.background2_pos)
-	# Back
-	for group in (self.buildings_back,):
+        # Back
+        for group in (self.buildings_back,):
             self.__draw_spritegroup(group)
-	# Middle
-	
-	for wave in self.waves:
+        # Middle
+
+        for wave in self.waves:
             if wave.distance <= 0:
                 self.__draw_spritegroup(wave)
-        self.__draw_spritegroup(self.pedestrians)
-	if self.player.sprite is not None:
+            self.__draw_spritegroup(self.pedestrians)
+        if self.player.sprite is not None:
             self.__draw_sprite(self.player.sprite._tractor_beam)	
         for group in (self.player,
                       self.player_bullets,
