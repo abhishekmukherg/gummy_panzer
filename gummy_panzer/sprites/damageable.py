3811be4060f4c3976974b5e80dea2e0d38b8c051
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-09-22 15:57:05 -0400

    Fixed health again

--- gummy_panzer/sprites/damageable.py
+++ gummy_panzer/sprites/damageable.py
@@ -5,8 +5,8 @@ class Damageable(object):
     """
     
     def __init__(self, max_health):
-        self.health = max_health
         self.max_health = max_health
+        self.health = max_health
 
     @property
     def health(self):
