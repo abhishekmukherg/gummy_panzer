21cf2ecb048547a233cc8015e131b1e84e3f7252
unknown <Administrator@.(none)>
2009-09-23 16:12:41 -0400

    Added some setup.py stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # bootstrap setuptools if necessary
 from ez_setup import use_setuptools
 use_setuptools()
+#from distutils.core import setup
 
 VERSION=0.1
 
@@ -9,5 +10,10 @@ from setuptools import setup
 setup(name="gummy_panzer",
         version=VERSION,
         packages=["gummy_panzer", "gummy_panzer.sprites"],
-        )
+        scripts=["main.py"],
+        install_requires = ['pygame'],
+        package_data = {
+            'gummy_panzer': ['images/*.png', 'Sounds/*.ogg'],
+        }
+)
 
