1b713cb30fa57ac0506072db59f18be005f6c6b8
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-09-23 02:05:41 -0400

    Added CHILLIN state for boss

--- gummy_panzer/sprites/boss.py
+++ gummy_panzer/sprites/boss.py
@@ -1,13 +1,29 @@
+import logging
 import pygame
+
 from gummy_panzer.sprites import damageable
 from gummy_panzer.sprites import effects
 from gummy_panzer.sprites import weapons
+
+
+LOG = logging.getLogger(__name__)
 
 
 BOSS_HEALTH = 100
 
 
 class Boss(pygame.sprite.Sprite, damageable.Damageable):
+
+    class State:
+        CHILLIN = 0
+        ATTACKING = 1
+        CREATING_GROUND = 2
+        CREATING_AIR = 3
+
+    STATE_TICKS = {State.CHILLIN: 10,
+                   State.ATTACKING: 10,
+                   State.CREATING_GROUND: 10,
+                   State.CREATING_AIR: 10}
 
     points = 1000
 
@@ -18,5 +34,18 @@ class Boss(pygame.sprite.Sprite, damageable.Damageable):
         self.image.fill((150, 150, 150))
         self.rect = self.image.get_rect()
         self.rect.topright = loc
+        self.state = Boss.State.CHILLIN
+        self.__state_tick = 0
+
+    def update(self):
+        if self.__state_tick >= Boss.STATE_TICKS[self.state]:
+            self.state = Boss.State.CHILLIN
+            self.__state_tick = 0
+            return
+        self.__state_tick += 1
+        if self.state == Boss.State.CHILLIN:
+            LOG.info("Chillin")
+
+    
 
         
